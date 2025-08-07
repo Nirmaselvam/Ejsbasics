61cc0fa24efd70f869059febd0004774782673aa
Nirmal <nirmalnirmal0123456@gmail.com>
2025-08-07 20:51:45 +0530

    passejs

---
+++ passejs.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+ const name = req.body.fName + req.body.lName;
+ const numberofLetters=
+ { numberofLetters: name.length
+ };
+
+
+ res.render ("index.ejs",numberofLetters);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
